7a1020c1c10e67c6e24d21acd3a3c7551e1322bd
caseuwen <casechen15@gmail.com>
2025-07-12 13:52:17 -0700

    fixing orc behavior script

--- Assets/scripts/enemy_orc_behavior.cs
+++ Assets/scripts/enemy_orc_behavior.cs
@@ -1,13 +1,12 @@
 using UnityEngine;
 
-public class enemy_orc_behavior : MonoBehaviour
+public class Enemy_orc_behavior : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public float speed = 2f;
     public Transform target;
     private float timer = 0f;
     private Animator animator;
-    private bool start_walking  = false;
     // Update is called once per frame
     void Start()
     {
@@ -18,11 +17,11 @@ public class enemy_orc_behavior : MonoBehaviour
         timer += Time.deltaTime;
         if (target == null || timer <= 2)
         {
-            animator.SetFloat("xVelocity", 0);
+            animator.SetFloat("Xvelocity", 0);
         }
         else
         {
-            animator.SetFloat("xVelocity", 2f);
+            animator.SetFloat("Xvelocity", 2f);
             transform.position = Vector2.MoveTowards(
             transform.position,
             target.position,
